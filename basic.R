0a9725e345f58aef7fc342934db807cfbe35f38e
jags6489 <78273051+jags6489@users.noreply.github.com>
2021-02-01 16:13:14 +0530

    Add files via upload

---
+++ basic.R
@@ -0,0 +1,12 @@
+df <- data.frame(height=c(150,160),weight=c(65,72))
+df
+df[1,2]
+df[1,]
+df[,1]
+df <- data.frame(x=c(1,2,3),y=c("a","b","c"))
+df
+df[1,1]
+df[1,]
+df[c(1,3),2]
+df[c(1,3),1]
+df[c(1,3),]
